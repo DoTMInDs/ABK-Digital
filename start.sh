1a43d4f0e6f389b0430c765ba81bf8f9dcf610c2
esmond <dhonniemccony3@gmail.com>
2026-02-02 05:24:54 -1200

    added requirement and start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+
+python mysite/manage.py collectstatic --no-input
+python mysite/manage.py migrate
+
+if [ "$DJANGO_CREATEUSER" == "1" ]; then 
+    python mysite/manage.py createsuperuser --noinput
+fi
+
+python mysite/manage.py runserver 0.0.0.0:$PORT
